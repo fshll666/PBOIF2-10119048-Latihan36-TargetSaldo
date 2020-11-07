481fdfb044e846f6298d63aeaac2b08a96417d61
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-07 14:33:18 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan36-TargetSaldo/src/pboif2/pkg10119048/latihan36/targetsaldo/PBOIF210119048Latihan36TargetSaldo.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan36.targetsaldo;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class PBOIF210119048Latihan36TargetSaldo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Target target = new Target();
+        
+        target.setSaldo(3500000);
+        target.setSaldoTarget(6000000);
+        target.setBunga(0.08);
+        
+        target.hitungTarget();
+        
+    }
+    
+}
